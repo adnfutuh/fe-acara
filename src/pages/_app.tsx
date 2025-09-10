38bc48714e0b651717cc4aaa066b27baea3122b5
rizki <rizkiadnanfutuh@gmail.com>
2025-09-11 02:25:55 +0700

    feat(app): add QueryClientProvider in _app.tsx for React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import "@/styles/globals.css";
 import { cn } from "@/utils/cn";
 import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
 
 const inter = Inter({
   subsets: ["latin"],
@@ -11,15 +21,17 @@ const inter = Inter({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <main
-        className={cn(
-          inter.className,
-          "flex min-h-screen min-w-full flex-col items-center justify-center gap-10 py-10 lg:py-0",
-        )}
-      >
-        <Component {...pageProps} />
-      </main>
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        <main
+          className={cn(
+            inter.className,
+            "flex min-h-screen min-w-full flex-col items-center justify-center gap-10 py-10 lg:py-0",
+          )}
+        >
+          <Component {...pageProps} />
+        </main>
+      </NextUIProvider>
+    </QueryClientProvider>
   );
 }
