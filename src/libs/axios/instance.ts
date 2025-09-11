043e6a33fca83b92df62beb6716550da6ae2c782
rizki <rizkiadnanfutuh@gmail.com>
2025-09-12 00:47:41 +0700

    feat(auth): add Login page with useLogin hook, form validation, and NextAuth integration

--- src/libs/axios/instance.ts
+++ src/libs/axios/instance.ts
@@ -1,31 +1,31 @@
 import environment from "@/config/environment";
+import { SessionExtended } from "@/types/Auth";
 import axios from "axios";
-import { error } from "console";
-import { Session } from "next-auth";
 import { getSession } from "next-auth/react";
 
-interface CustomSession extends Session {
-  accessToken?: string;
-}
 const headers = {
   "Content-Type": "application/json",
 };
-const instance = axios.create({ baseURL: environment.API_URL, headers, timeout: 60 * 1000 });
+const instance = axios.create({
+  baseURL: environment.API_URL,
+  headers,
+  timeout: 60 * 1000,
+});
 
 instance.interceptors.request.use(
   async (request) => {
-    const session: CustomSession | null = await getSession();
+    const session: SessionExtended | null = await getSession();
     if (session && session.accessToken) {
       request.headers.Authorization = `Bearer ${session.accessToken}`;
     }
     return request;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
 instance.interceptors.response.use(
   (response) => response,
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
 export default instance;
